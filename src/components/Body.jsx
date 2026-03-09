171f6ff4b119e4d69b1f8fca1d9da6052eb1b4a9
patoari <17225210321@nu.ac.bd>
2026-03-09 22:54:29 +0600

    upload Body

---
+++ src/components/Body.jsx
@@ -0,0 +1,10 @@
+function Body(){
+    return(
+        <div>
+            <h2>Main Content</h2>
+            <p>This is main area of the website</p>
+            <p>Here you can show articles, lessons, or other Content</p>
+        </div>
+    );
+}
+export default Body;
