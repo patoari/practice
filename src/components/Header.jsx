a505e628794a6f9a579be409a77ac997eacda05a
patoari <17225210321@nu.ac.bd>
2026-03-09 22:04:44 +0600

    Update

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <header>
+        <h1>My Learning Website</h1>
+        <p>welcome to our React practice project</p>
+        hr
+        </header>
+    )
+}
