63ac6320a81876c5b8f46925ed706938b4e8fc68
patoari <17225210321@nu.ac.bd>
2026-03-09 22:16:25 +0600

    upload footer

---
+++ src/components/Leftbar.jsx
@@ -0,0 +1,14 @@
+function Leftbar(){
+    return(
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>Home</li>
+                <li>Course</li>
+                <li>Students</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    );
+}
+export default Leftbar;
